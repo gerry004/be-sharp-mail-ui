852f414438251c46e77c63e20a272ec78463e21a
Gerry Yang <gerry04y@gmail.com>
2024-02-05 17:10:47 +0000

    Axios for Deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+let baseURL = 'http://localhost:5000';
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://be-sharp-mail-api.onrender.com';
+}
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: baseURL,
 });
 
 export default api;
